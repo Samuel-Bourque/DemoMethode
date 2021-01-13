cf1a572b6c509304dacf89bb4ada86a3978d1698
Samuel Bourque <70170253+Samuel-Bourque@users.noreply.github.com>
2021-01-13 01:04:26 -0500

    Add files via upload

---
+++ DemoMethodes_UI/DemoMethodes_UI/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DemoMethodes_UI
+{
+    /// <summary>
+    /// Logique d'interaction pour App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            Thread.CurrentThread.CurrentCulture =
+                   new System.Globalization.CultureInfo("en-US");
+            Thread.CurrentThread.CurrentUICulture =
+                    new System.Globalization.CultureInfo("en-US");
+        }
+    }
+}
---
+++ DemoMethodes_UI/DemoMethodes_UI/ConvertisseurDoubleString.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace DemoMethodes_UI
+{
+    [ValueConversion(typeof(string), typeof(double))]
+
+    public class ConvertisseurDoubleString : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double num;
+            string chaine = value as string;
+            if (double.TryParse(chaine, out num) && !chaine.EndsWith("."))
+            {
+                return num; // auto-boxing (transtypage vers objet Double automatique)
+            }
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
+
